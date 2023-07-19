6f1555e82b48d6bf47f9c757943a7ba625868454
Denzel18rb <74129875rb@gmail.com>
2023-07-19 14:31:50 -0700

    0. My first square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+ESTE MODULO DEFINE UNA CALSE CUADRADA VACIA QUE REPRESENTA UN CUADRADO :D.
+"""
+
+
 class Square:
-    def __init__(self):
-        pass
+    """
+    ESTO REPRESENTA UNA CLASE CUADRADA
+    UN CUADADO SIN ATRIBUTOS NI METODOS DEFINIDOS
+    """
+    pass
