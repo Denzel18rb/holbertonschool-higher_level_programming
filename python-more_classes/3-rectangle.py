96ff18a73fef32d4f6998d773907180992568bde
Denzel18rb <135359331+Denzel18rb@users.noreply.github.com>
2023-07-30 14:17:01 -0500

    3. String representation

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """
-Area and Perimeter =)
+this module define a class rectangle.
 """
 
 
 class Rectangle:
-    """New class Rectangle"""
+    """
+    Rectangle with attrubutes.
+    """
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
@@ -14,33 +16,42 @@ class Rectangle:
     def width(self):
         return self.__width
 
+    @property
+    def height(self):
+        return self.__height
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
-
-    @property
-    def height(self):
-        return self.__height
+        self.__width = value
 
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
 
     def area(self):
-        return self.__height * self.__width
+        return self.width * self.height
 
     def perimeter(self):
-        if self.__width == 0 or self.__height == 0:
+        if self.width == 0 or self.height == 0:
             return 0
         else:
-            return 2 * (self.__width + self.__height)
+            return 2 * (self.width + self.height)
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
+
+        accumulator = ""
+        for i in range(self.height):
+            for j in range(self.width):
+                accumulator += "#"
+            accumulator += "\n"
+        return accumulator[:-1]
