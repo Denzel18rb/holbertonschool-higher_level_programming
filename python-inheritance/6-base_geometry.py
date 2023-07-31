be928811486516fdb5d9dad64075e702edf52ea4
Denzel18rb <135359331+Denzel18rb@users.noreply.github.com>
2023-07-30 20:39:28 -0500

    6. Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+this module define a class geometry
+"""
+
+
+class BaseGeometry:
+    """
+    this class define a method
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
