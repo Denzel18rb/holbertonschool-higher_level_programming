93efdb28525744cc4ab7f1f8507368bf8602f3ef
Denzel18rb <135359331+Denzel18rb@users.noreply.github.com>
2023-08-27 17:56:55 -0500

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
-""" Lists all states from the database hbtn_0e_0_usa """
-import sys
+""" lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
+import sys
 
+""" To not execute when imported """
 if __name__ == '__main__':
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    db = sys.argv[3]
-    HOST = 'localhost'
 
-    db = MySQLdb.connect(host=HOST, user=user, passwd=passwd, db=db, port=3306)
+    """ Make a connection with the database """
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
 
-    pointer = db.cursor()
-    pointer.execute("SELECT * FROM states;")
-    states = pointer.fetchall()
+    """ Creating cursor """
+    c = db.cursor()
 
-    for state in states:
-        print(state)
+    """ Execute queries """
+    query = "SELECT * FROM states ORDER BY id"
+    c.execute(query)
 
-    pointer.close()
+    """ Printing all the rows """
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    """ Cleaning process """
+    c.close()
     db.close()
