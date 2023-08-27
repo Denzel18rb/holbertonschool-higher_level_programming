f754eac9a08c956715b35462288a0dc77b989788
Denzel18rb <135359331+Denzel18rb@users.noreply.github.com>
2023-08-27 11:57:52 -0500

    Update model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-''' Script to create a model '''
+""" python file that contains the class definition of a State and an instance
+"""
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
 
-Base = declarative_base()
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
 
 
 class State(Base):
-    ''' State model '''
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True)
+    """ Class state that inherient of the Base"""
+    __tablename__ = "states"
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
