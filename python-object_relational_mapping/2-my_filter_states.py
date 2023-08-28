50f268b72a601661eba141046b0358504620d046
Denzel18rb <74129875rb@gmail.com>
2023-08-27 18:13:37 -0700

    pycodestyle

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -5,7 +5,8 @@ import MySQLdb
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+        print("Usage: {} <username> <password> <database> <state_name>"
+              .format(sys.argv[0]))
         sys.exit(1)
 
     username = sys.argv[1]
@@ -20,18 +21,13 @@ if __name__ == "__main__":
             user=username,
             passwd=password,
             db=database_name
-        )
-
+            )
         cur = conn.cursor()
-
         query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
         cur.execute(query, (state_name,))
-
         states = cur.fetchall()
-
         for state in states:
             print(state)
-
         cur.close()
         conn.close()
 
