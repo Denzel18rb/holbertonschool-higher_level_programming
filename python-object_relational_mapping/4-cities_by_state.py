412c986ba753ef102e086b274ec5d384ff1cbe20
Denzel18rb <135359331+Denzel18rb@users.noreply.github.com>
2023-08-27 20:32:27 -0500

    Update 4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -7,13 +7,12 @@ import sys
 def list_cities(username, password, database_name):
     try:
         db = MySQLdb.connect(host="localhost", port=3306,
-            user=username, passwd=password, db=database_name)
-        
+                             user=username, passwd=password, db=database_name)
         cursor = db.cursor()
-        cursor.execute("SELECT cities.id, cities.name, states.name \
-                FROM cities \
-                JOIN states ON cities.state_id = states.id \
-                ORDER BY cities.id ASC")
+        cursor.execute("SELECT cities.id, cities.name, states.name "
+                       "FROM cities "
+                       "JOIN states ON cities.state_id = states.id "
+                       "ORDER BY cities.id ASC")
 
         cities = cursor.fetchall()
         for city in cities:
@@ -31,6 +30,10 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: python script.py <username> <password> <database_name>")
         sys.exit(1)
+
+    username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    list_cities(username, password, database_name)
+
 
     username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
     list_cities(username, password, database_name)
