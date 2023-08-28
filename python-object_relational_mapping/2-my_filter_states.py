4b44ded6655a72c89b122ac7e277d27f22c7cf3d
Denzel18rb <135359331+Denzel18rb@users.noreply.github.com>
2023-08-27 20:14:56 -0500

    Update 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -5,7 +5,8 @@ import MySQLdb
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+        print("Usage: {} <username> <password> <database> <state_name>"
+              .format(sys.argv[0]))
         sys.exit(1)
 
     username = sys.argv[1]
@@ -21,7 +22,6 @@ if __name__ == "__main__":
             passwd=password,
             db=database_name
         )
-    
         cur = conn.cursor()
         query = "SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC"
         cur.execute(query, (state_name,))
