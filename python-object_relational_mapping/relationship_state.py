428d373175a8eac6aa58d3d83dc00c26b4591397
Denzel18rb <135359331+Denzel18rb@users.noreply.github.com>
2023-08-27 21:09:17 -0500

    Update relationship_state.py

--- python-object_relational_mapping/relationship_state.py
+++ python-object_relational_mapping/relationship_state.py
@@ -5,12 +5,10 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from relationship_city import Base, City
 
-
 class State(Base):
-    "Represents a State"
-    __tablename__ = "states"
-    id = Column(Integer, autoincrement=True, unique=True,
-                nullable=False, primary_key=True)
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
-	
+	"Represents a State"
+	__tablename__ = "states"
+	id = Column(Integer, autoincrement=True, unique=True,
+		nullable=False, primary_key=True)
+	name = Column(String(128), nullable=False)
+	cities = relationship("City", backref="state", cascade="all, delete")
