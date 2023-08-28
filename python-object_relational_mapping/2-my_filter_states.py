aacb4d93a2c3db63b9dff5b414be9bc117a2d32d
Denzel18rb <135359331+Denzel18rb@users.noreply.github.com>
2023-08-27 19:56:38 -0500

    Update 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -21,17 +21,13 @@ if __name__ == "__main__":
             passwd=password,
             db=database_name
         )
-
+    
         cur = conn.cursor()
-
-        query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+        query = "SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC"
         cur.execute(query, (state_name,))
-
         states = cur.fetchall()
-
         for state in states:
             print(state)
-
         cur.close()
         conn.close()
 
